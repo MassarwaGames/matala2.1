231c409fc87edcaa4ea541345be9f1da10632913
Abed El Kareem Massarwa <abodi.msarwi@gmail.com>
2024-11-19 22:46:31 +0200

    corrected hider 🌿

--- Assets/Hider.cs
+++ Assets/Hider.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Hider : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Tooltip("Key to toggle visibility")]
+    [SerializeField] private KeyCode toggleKey = KeyCode.H;
     void Start()
     {
         
@@ -11,6 +13,10 @@ public class Hider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(toggleKey))
+        {
+            // Properly toggle the active state
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
     }
 }
